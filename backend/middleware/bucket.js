f646168ec061d676a022dc2696fe48527268e7af
mortagh <ax.geremia@gmail.com>
2023-04-23 20:30:27 +0200

    minio okay

--- backend/middleware/bucket.js
+++ backend/middleware/bucket.js
@@ -1,24 +1,23 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const AWS = require('@aws-sdk/client-s3')
+const AWS = require('aws-sdk')
 
 require('dotenv').config();
 
 const bucket = "memes-bucket"
 const port = 9000
-const host = process.env.SQL_HOST
+const host = process.env.MINIO_HOST
 
+//service qui ce connecte au bucket
 const s3 = new AWS.S3({
     endpoint: `http://${host}:${port}`,
-    credentials : {
-        accessKeyId:process.env.MINIO_ROOT_USER,
-        secretAccessKey: process.env.MINIO_ROOT_PASSWORD,
-
-    },
+    accessKeyId:process.env.MINIO_ROOT_USER,
+    secretAccessKey: process.env.MINIO_ROOT_PASSWORD,
     sslEnabled: false,
     s3ForcePathStyle: true,
-    region: 'us-east-1',
+    region: 'eu-west-3',
   });
+
 
 const storage = multerS3({
     s3,
