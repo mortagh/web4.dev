4f31a72784ba9db4a599498f3ed7f31da6677710
laura-lpn <74555001+laura-lpn@users.noreply.github.com>
2023-05-23 20:45:53 +0200

    jimp text

--- backend/routes/create.js
+++ backend/routes/create.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { upload, s3 } = require('../middleware/bucket');
 const Jimp = require("jimp");
 const { dbConnect } = require('../module');
+const path =require('path')
 
 router.post("/", upload.single("image"), async (req, res) => {
   const image = req.file;
@@ -11,11 +12,33 @@ router.post("/", upload.single("image"), async (req, res) => {
   const name = req.body.name;
   const key = Date.now() + image.originalname;
 
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+  // let fPath = path.resolve(__dirname,'font/font.fnt')
+  let fPath = path.resolve(Jimp.FONT_SANS_32_WHITE)
+
+  const font = await Jimp.loadFont(fPath);
   const jimpImage = await Jimp.read(Buffer.from(image.buffer, "base64"));
 
-  jimpImage.print(font, 10, 0, topText);
-  jimpImage.print(font, 10, 10, bottomText);
+  var w = jimpImage.bitmap.width;
+  var h = jimpImage.bitmap.height;
+  var topTextWidth = Jimp.measureText(font, topText);
+  var topTextHeight = Jimp.measureTextHeight(font, topText);
+
+  var bottomTextWidth = Jimp.measureText(font, bottomText);
+  var bottomTextHeight = Jimp.measureTextHeight(font, bottomText);
+  jimpImage.print(
+    font, w/2 - topTextWidth/2, 0,
+    {
+      text: topText,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_TOP,
+  },topTextWidth, topTextHeight);
+  jimpImage.print(
+    font, w/2 - bottomTextWidth/2, h - bottomTextHeight,
+    {
+      text: bottomText,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM,
+  },bottomTextWidth, bottomTextHeight);
 
   const params = {
     Bucket: process.env.BUCKET_NAME,
