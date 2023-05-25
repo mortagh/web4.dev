4c44b2535b6cda726369d09a3c2b70fb6a08a23f
AxelGeremia <ax.geremia@gmail.com>
2023-05-25 14:33:09 +0200

    multi select + fileInput OK

--- frontend/src/components/FileInput.vue
+++ frontend/src/components/FileInput.vue
@@ -1,16 +1,7 @@
 <template>
     <div>
         <label class="block mb-2 text-base text-white" for="file_input">{{name}}</label>
-        <!-- <input required @change="$emit('file-selected', $event.target.files[0])"
-        class="block w-full
-        text-purple font-base bg-white
-        px-3 pl-0
-        overflow-hidden
-        outline-none
-        border-black border-2 rounded-input cursor-pointer"
-         id="file_input" type="file" accept="image/png, image/jpeg" > -->
-        <input required
-        :value="modelValue" @input="$emit('update:modelValue', $event.target.files[0])"
+        <input required @change="$emit('file-selected', $event.target.files[0])"
         class="block w-full
         text-purple font-base bg-white
         px-3 pl-0
@@ -18,6 +9,15 @@
         outline-none
         border-black border-2 rounded-input cursor-pointer"
          id="file_input" type="file" accept="image/png, image/jpeg" >
+        <!-- <input required
+        :value="modelValue" @input="$emit('update:modelValue', $event.target.files[0])"
+        class="block w-full
+        text-purple font-base bg-white
+        px-3 pl-0
+        overflow-hidden
+        outline-none
+        border-black border-2 rounded-input cursor-pointer"
+         id="file_input" type="file" accept="image/png, image/jpeg" > -->
     </div>
 
 </template>
